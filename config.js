9d6152b84936f50f35e23e098306f4f448ebb4f1
fr4nkl1 <144033485+fr4nkl1@users.noreply.github.com>
2024-01-29 18:32:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['2349090854340', 'K3L3CH1', true],
+  ['2349090854340', 'N1GG4', true], 
   [''] 
 ] //Number of owners
 
